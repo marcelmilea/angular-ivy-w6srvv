db426fc37d11114484466774ded61010ed85875b
marcelmilea <marcel.milea@gmail.com>
2022-06-07 13:40:57 +0200

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,102 @@
 import { Component, VERSION } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Candidate';
+  submitted = false;
+  invitee = '';
+
+  colleagues = new FormControl('');
+  colleaguesList: string[] = ["I'm bringing some colleagues", 'Just myself'];
+  selectedColleagues = '';
+
+  inviteeForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    phone: '',
+    email: [null, [Validators.required, Validators.email]],
+    gender: [''],
+    answer: [''],
+    selectedColleagues: '',
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+    this.buildAcceptForm();
+    this.submitted = false;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    // Process checkout data here
+    this.invitee = 'Gigi';
+
+    window.alert(
+      'The product has been shared! ' +
+        this.inviteeForm.value.selectedColleagues
+    );
+
+    if (this.inviteeForm.valid) {
+      console.log(this.inviteeForm.value); // Process your form
+      this.inviteeForm.reset();
+    }
+  }
+
+  buildAcceptForm() {
+    this.inviteeForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      phone: [
+        null,
+        [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')],
+      ],
+      email: [null, [Validators.required, Validators.email]],
+      gender: ['', Validators.required],
+      answer: ['', Validators.required],
+      selectedColleagues: ['', Validators.required],
+    });
+    this.inviteeForm.updateValueAndValidity;
+  }
+
+  buildRefuseForm() {
+    this.inviteeForm = this.formBuilder.group({
+      name: '',
+      phone: '',
+      email: '',
+      gender: '',
+      answer: '',
+      selectedColleagues: '',
+    });
+    this.inviteeForm.updateValueAndValidity;
+  }
+
+  // setUserCategoryValidators() {
+  //   const name = this.inviteeForm.get('phone');
+  //   const email = this.inviteeForm.get('email');
+  //   const gender = this.inviteeForm.get('gender');
+  //   const answer = this.inviteeForm.get('answer');
+  //   const selectedColleagues = this.inviteeForm.get('selectedColleagues');
+
+  //   this.inviteeForm.get('answer').valueChanges
+  //     .subscribe(answer => {
+
+  //       if (answer === 'yes') {
+  //         institutionControl.setValidators([Validators.required]);
+  //         companyControl.setValidators(null);
+  //         salaryControl.setValidators(null);
+  //       }
+
+  //       if (answer === 'no') {
+  //         institutionControl.setValidators(null);
+  //         companyControl.setValidators([Validators.required]);
+  //         salaryControl.setValidators([Validators.required]);
+  //       }
+
+  //       institutionControl.updateValueAndValidity();
+  //       companyControl.updateValueAndValidity();
+  //       salaryControl.updateValueAndValidity();
+  //     });
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonToggleModule,
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
